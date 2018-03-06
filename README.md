693d74ec76d6b03a4fb62c03e65686617bad3b9b
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2018-03-06 09:39:21 +0800

    :tada: Basic shell that outputs what it reads, with exit functionality

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # haww
+
 Elementary shell in C
+
+## How to run
+
+- Create a `build` directory inside this repository.
+- Run `cmake ..` in the `build` directory.
+- Run `make`
+- `haww` is created, you can use it like this: `./haww`
+
+---
+
+## References
+
+- [Write a shell in C](https://brennan.io/2015/01/16/write-a-shell-in-c/)
+- [Making your own Linux Shell in C](https://www.geeksforgeeks.org/making-linux-shell-c/)
+- [simple-c-shell](https://github.com/jmreyes/simple-c-shell)
---
+++ src/main.c
@@ -0,0 +1,36 @@
+//
+// Created by manparvesh on 6/3/2018.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define TRUE 1
+
+void main_loop();
+
+char *read_line();
+
+int main() {
+    main_loop();
+    return EXIT_SUCCESS;
+}
+
+void main_loop() {
+    while (TRUE) {
+        printf(">>> ");
+        char *input_command = read_line();
+        if (strncmp(input_command, "exit", 4) == 0) {
+            break;
+        }
+        fputs(input_command, stdout);
+    }
+}
+
+char *read_line() {
+    char *line = NULL;
+    ssize_t buffer_size = 0;
+    getline(&line, &buffer_size, stdin);
+    return line;
+}
